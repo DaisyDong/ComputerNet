74db02995e8e45bba1be08fedaf6ce93b21a2d2b
Daisy <1723767242@qq.com>
2015-05-27 15:44:06 +0800

    实现缓存以及钓鱼网站和屏蔽网站

--- proxy1/src/test/test.java
+++ proxy1/src/test/test.java
@@ -5,15 +5,15 @@ import java.io.*;
 public class test {
 	public static void main(String args[]) { 
 		try {
-			Socket socket = new Socket("www.renren.com",80);
+			Socket socket = new Socket("icode.renren.com",80);
 			InputStream is = socket.getInputStream();
 			BufferedReader br = new BufferedReader(new InputStreamReader(is));
 			OutputStream os = socket.getOutputStream();
 			PrintWriter pw = new PrintWriter(os);
 			
-			pw.write("GET /a72842/n/core/base-all2.js HTTP/1.1\r\n");
-			pw.write("Host: s.xnimg.cn\r\n");
-			pw.write("If-modified-since: Wed, 12 Nov 2014 03:36:04 GMT\r\n");
+			pw.write("GET http://ebp.renren.com/ebpn/show?ref=http://www.renren.com/ad_100000000061&r=http%3A%2F%2Fwww.renren.com%2FSysHome.do HTTP/1.1\r\n");
+			pw.write("Host: ebp.renren.com\r\n");
+			pw.write("If-modified-since: Tue, 06 Mar 2012 08:41:41 GMT\r\n");
 			pw.write("\r\n");
 			pw.flush();
 			
